6a76accc9b7eef98b750175e32da41ac72eb16e3
Flores Aguilar Gael Ricardo <mygtgael@hotmail.com>
2018-04-25 15:46:00 +0000

    se agrega la variable para el incremento

--- ejemplo-1-calentador.cpp
+++ ejemplo-1-calentador.cpp
@@ -4,18 +4,21 @@ class Calentador
 {
     private: 
     int temperatura;
+    int incremento;
     
     public:
     Calentador();
     void calentar();
     void enfriar();
     void imprimeTempertura();
+    int accedeTemperatura() const;
     
 };//punto y coma obligatorio, es aprte dela sintaxis
 
 Calentador::Calentador()
 {
     temperatura = 15;
+    incremento = 3;
 }
 
 void Calentador::calentar()
@@ -28,9 +31,14 @@ void Calentador::enfriar()
     temperatura -=5;
 }
 
-void Calentador::imprimeTempertura()
+void Calentador::imprimeTempertura()//se debe eindicar a que classe pertenece el metodo, se hace con (clase::nombre del metod)
 {
     std::cout << "La temperatura es: " << temperatura << std::endl;
+}
+
+int Calentador::accedeTemperatura() const
+{
+    return temperatura;
 }
 
 int main()
